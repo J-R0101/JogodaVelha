ea404a3bc4cdaf9f32be163640f0d1e089576dbe
J-R0101 <jrjose@outlook.com>
2021-05-18 21:42:41 -0300

    jogovelha

---
+++ velha/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace velha {
+    public partial class Form1 : Form {
+        bool xies = true;
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            B11.Click += new EventHandler(BClick);
+            B12.Click += new EventHandler(BClick);
+            B13.Click += new EventHandler(BClick);
+            B21.Click += new EventHandler(BClick);
+            B22.Click += new EventHandler(BClick);
+            B23.Click += new EventHandler(BClick);
+            B31.Click += new EventHandler(BClick);
+            B32.Click += new EventHandler(BClick);
+            B33.Click += new EventHandler(BClick);
+            foreach (Control item in this.Controls) {
+                if ( item is Button) {
+                    item.TabStop = false ;
+                }
+            }
+        }
+        private void BClick(object sender, EventArgs e) {
+            ((Button)sender).Text = this.xies ? "x" : "o";
+            ((Button)sender).Enabled = false;
+
+            VerificarGanhador();
+
+            xies = !xies;
+
+            defalt.Text = String.Format("{0}, é sua vez", this.xies ? "x" : "o");
+        }
+
+        private void VerificarGanhador() {
+            if (
+             B11.Text != String.Empty && B11.Text == B12.Text && B12.Text == B13.Text ||
+             B21.Text != String.Empty && B21.Text == B22.Text && B22.Text == B23.Text ||
+             B31.Text != String.Empty && B31.Text == B32.Text && B32.Text == B33.Text ||
+
+             B11.Text != String.Empty && B11.Text == B21.Text && B21.Text == B31.Text ||
+             B12.Text != String.Empty && B12.Text == B22.Text && B22.Text == B32.Text ||
+             B13.Text != String.Empty && B13.Text == B23.Text && B23.Text == B33.Text ||
+
+             B11.Text != String.Empty && B11.Text == B22.Text && B22.Text == B33.Text ||
+             B13.Text != String.Empty && B13.Text == B22.Text && B22.Text == B31.Text 
+            ) {
+                MessageBox.Show(String.Format("O ganhador é: {0}", xies ? "x" : "o"), "Temos um vitorioso", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Reiniciar();
+            }
+           else {
+                VerificarEmpate();
+            }
+        }
+
+        private void VerificarEmpate() {
+            bool TodosDesabilitados = true;
+            foreach(Control item in this.Controls) {
+                if(item is Button && item.Enabled) {
+                    TodosDesabilitados = false;
+                    break;
+                }
+            }
+            if (TodosDesabilitados) {
+                MessageBox.Show(String.Format("Deu empate"), "Ops!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Reiniciar();
+            }
+            
+        }
+
+        private void Reiniciar() {
+            foreach(Control item in this.Controls) {
+                if(item is Button) {
+                    item.Enabled = true;
+                    item.Text = String.Empty;
+                }
+            }
+        }
+    }
+}
---
+++ velha/velha/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace velha {
+    partial class Form1 {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.B11 = new System.Windows.Forms.Button();
+            this.defalt = new System.Windows.Forms.Label();
+            this.B33 = new System.Windows.Forms.Button();
+            this.B32 = new System.Windows.Forms.Button();
+            this.B31 = new System.Windows.Forms.Button();
+            this.B23 = new System.Windows.Forms.Button();
+            this.B22 = new System.Windows.Forms.Button();
+            this.B21 = new System.Windows.Forms.Button();
+            this.B13 = new System.Windows.Forms.Button();
+            this.B12 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // B11
+            // 
+            this.B11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B11.ForeColor = System.Drawing.Color.Red;
+            this.B11.Location = new System.Drawing.Point(31, 58);
+            this.B11.Name = "B11";
+            this.B11.Size = new System.Drawing.Size(50, 50);
+            this.B11.TabIndex = 0;
+            this.B11.UseVisualStyleBackColor = true;
+            // 
+            // defalt
+            // 
+            this.defalt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.defalt.ForeColor = System.Drawing.Color.Red;
+            this.defalt.Location = new System.Drawing.Point(31, 9);
+            this.defalt.Name = "defalt";
+            this.defalt.Size = new System.Drawing.Size(203, 46);
+            this.defalt.TabIndex = 9;
+            this.defalt.Text = "X, e a sua vez";
+            this.defalt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // B33
+            // 
+            this.B33.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B33.ForeColor = System.Drawing.Color.Red;
+            this.B33.Location = new System.Drawing.Point(184, 191);
+            this.B33.Name = "B33";
+            this.B33.Size = new System.Drawing.Size(50, 50);
+            this.B33.TabIndex = 11;
+            this.B33.UseVisualStyleBackColor = true;
+            // 
+            // B32
+            // 
+            this.B32.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B32.ForeColor = System.Drawing.Color.Red;
+            this.B32.Location = new System.Drawing.Point(104, 191);
+            this.B32.Name = "B32";
+            this.B32.Size = new System.Drawing.Size(50, 50);
+            this.B32.TabIndex = 12;
+            this.B32.UseVisualStyleBackColor = true;
+            // 
+            // B31
+            // 
+            this.B31.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B31.ForeColor = System.Drawing.Color.Red;
+            this.B31.Location = new System.Drawing.Point(31, 191);
+            this.B31.Name = "B31";
+            this.B31.Size = new System.Drawing.Size(50, 50);
+            this.B31.TabIndex = 13;
+            this.B31.UseVisualStyleBackColor = true;
+            // 
+            // B23
+            // 
+            this.B23.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B23.ForeColor = System.Drawing.Color.Red;
+            this.B23.Location = new System.Drawing.Point(184, 124);
+            this.B23.Name = "B23";
+            this.B23.Size = new System.Drawing.Size(50, 50);
+            this.B23.TabIndex = 14;
+            this.B23.UseVisualStyleBackColor = true;
+            // 
+            // B22
+            // 
+            this.B22.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B22.ForeColor = System.Drawing.Color.Red;
+            this.B22.Location = new System.Drawing.Point(104, 124);
+            this.B22.Name = "B22";
+            this.B22.Size = new System.Drawing.Size(50, 50);
+            this.B22.TabIndex = 15;
+            this.B22.UseVisualStyleBackColor = true;
+            // 
+            // B21
+            // 
+            this.B21.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B21.ForeColor = System.Drawing.Color.Red;
+            this.B21.Location = new System.Drawing.Point(31, 124);
+            this.B21.Name = "B21";
+            this.B21.Size = new System.Drawing.Size(50, 50);
+            this.B21.TabIndex = 16;
+            this.B21.UseVisualStyleBackColor = true;
+            // 
+            // B13
+            // 
+            this.B13.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B13.ForeColor = System.Drawing.Color.Red;
+            this.B13.Location = new System.Drawing.Point(184, 58);
+            this.B13.Name = "B13";
+            this.B13.Size = new System.Drawing.Size(50, 50);
+            this.B13.TabIndex = 17;
+            this.B13.UseVisualStyleBackColor = true;
+            // 
+            // B12
+            // 
+            this.B12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B12.ForeColor = System.Drawing.Color.Red;
+            this.B12.Location = new System.Drawing.Point(104, 58);
+            this.B12.Name = "B12";
+            this.B12.Size = new System.Drawing.Size(50, 50);
+            this.B12.TabIndex = 18;
+            this.B12.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 253);
+            this.Controls.Add(this.B12);
+            this.Controls.Add(this.B13);
+            this.Controls.Add(this.B21);
+            this.Controls.Add(this.B22);
+            this.Controls.Add(this.B23);
+            this.Controls.Add(this.B31);
+            this.Controls.Add(this.B32);
+            this.Controls.Add(this.B33);
+            this.Controls.Add(this.defalt);
+            this.Controls.Add(this.B11);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da Velha";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button B11;
+        private System.Windows.Forms.Label defalt;
+        private System.Windows.Forms.Button B33;
+        private System.Windows.Forms.Button B32;
+        private System.Windows.Forms.Button B31;
+        private System.Windows.Forms.Button B23;
+        private System.Windows.Forms.Button B22;
+        private System.Windows.Forms.Button B21;
+        private System.Windows.Forms.Button B13;
+        private System.Windows.Forms.Button B12;
+    }
+}
+
